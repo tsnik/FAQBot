7606385f054db883ac82216c5c5a82fcc910cc93
Nikita Tsyrlin <nstsyrlin@gmail.com>
2017-11-11 15:23:55 +0300

    Create client class

---
+++ FaqBotServer/FaqBotServer/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaqBotServer
+{
+    class Client
+    {
+        private int uid;
+        private int qid;
+
+        public void OnMessage(String Text)
+        {
+            return;
+        }
+    }
+}
