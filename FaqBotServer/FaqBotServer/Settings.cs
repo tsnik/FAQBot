ba9cab71af17411b1fdb5e5736a67fc746239f41
Nikita Tsyrlin <nstsyrlin@gmail.com>
2017-11-11 14:53:39 +0300

    Inital project structure

---
+++ FaqBotServer/FaqBotServer/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaqBotServer
+{
+    struct DBCredentials
+    {
+        String ServerName;
+        String DBName;
+        String User;
+        String Password;
+    }
+    class Settings
+    {
+        private String apiKey;
+        private String supportEmail;
+        private DBCredentials dbCreds;
+
+        public String ApiKey
+        {
+            get
+            {
+                return apiKey;
+            }
+        }
+
+        public String SupportEmail
+        {
+            get
+            {
+                return supportEmail;
+            }
+        }
+
+        public DBCredentials DBCreds
+        {
+            get
+            {
+                return dbCreds;
+            }
+        }
+
+        public void LoadSettings()
+        {
+            return;
+        }
+
+    }
+}
