e51680def4487642b66ff0a376a1a89a2f7d6215
Nikita Tsyrlin <nstsyrlin@gmail.com>
2017-11-11 15:18:36 +0300

    Create DB class

--- FaqBotServer/FaqBotServer/QuestionsBase.cs
+++ FaqBotServer/FaqBotServer/QuestionsBase.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace FaqBotServer
 {
+    enum AnswerType
+    {
+        Category,
+        Answer,
+        Other
+    }
+
+    struct Answer
+    {
+        AnswerType Type;
+        String Text;
+        int id;
+    }
     class QuestionsBase
     {
+        public void LoadDB()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Вернёт список, если есть несколько ккатегорий на этом уровне
+        /// Вернет список из одного элемента, если достигли самого нижнего уровня
+        /// </summary>
+        /// <param name="pos">Позиция элемента в списке. Для верхнего уровня -1.</param>
+        /// <param name="id">Идентификатор родителя. Для верхнего уровня -1.</param>
+        /// <returns></returns>
+        public List<Answer> GetAnswer(int pos=-1, int id=-1)
+        {
+            return null;
+        }
     }
 }
